e871beec3c11fe371581284d49e4da51029b4a67
mathidc <m.frua.3995@vallauri.edu>
2026-07-23 10:51:17 +0200

    Add files via upload
    implementato funzionamento base di battaglia navale (versione basilare)

--- Compiti_vacanze/battagliaNavale/script.js
+++ Compiti_vacanze/battagliaNavale/script.js
@@ -1,8 +1,52 @@
 let campoGioco = document.getElementById("grigliaGioco");
 let campoStatistiche = document.getElementById("campoStatistiche");
+let idTentativi = document.getElementById("numTentativi");
+let numColpi = document.getElementById("numColpi");
+let idTempoS = document.getElementById("tempoSec");
+let idTempoM = document.getElementById("tempoMin");
+let idParTempo = document.getElementById("paragrafoTempo");
+let par = document.getElementById("paragrafo");
+let btnNuovaPartita = document.getElementById("btnNuovaPartita");
 let cont = 10;
 let tentativi = 0;
+let naviColpite = 0;
+let idTempo;
+let sec = 0;
+let min = 0;
 window.onload = function () {
+
+    btnNuovaPartita.addEventListener("click", avviaPartita)
+
+    function avviaPartita(){
+        location.reload();
+    }
+
+    idTempo = setInterval(function(){
+        sec++
+        if(sec >= 60)
+        {
+            min++;
+            sec = 0;
+        }
+
+        if(sec <= 9)
+        {
+            idTempoS.innerHTML = "0" + sec;
+        }
+        else
+        {
+            idTempoS.innerHTML = sec;
+        }
+        if(min <= 9)
+        {
+            idTempoM.innerHTML = "0" + min;
+        }
+        else
+        {
+            idTempoM.innerHTML = min;
+        }
+    },1000)
+
     for(let i = 0; i < 10; i++)
     {
         let tr = document.createElement("div");
@@ -19,6 +63,7 @@ window.onload = function () {
                 cont--;
                 td.nave = "nave";
                 console.log(td);
+                naviColpite++;
             }
             else
             {
@@ -30,6 +75,8 @@ window.onload = function () {
             tr.appendChild(td);
         }
     }
+
+    numColpi.innerHTML = naviColpite;
     function affonda() {
         let idNat = this.id;
         idNat = idNat.split("-");
@@ -39,14 +86,35 @@ window.onload = function () {
         if(this.nave == "nave")
         {
             this.classList.add("bg-danger");
-
+            naviColpite--;
+            numColpi.innerHTML = naviColpite;
+            if(naviColpite <= 0)
+            {
+                par.innerHTML = "Congratulazioni, hai affondato tutte le navi in " + tentativi + " tentativi";
+                idParTempo.innerHTML = "Tempo usato: " + min + ":" + sec
+                clearInterval(idTempo);
+                disabilataCelle();
+            }
         }
         else
         {
             this.classList.add("bg-primary");
         }
+        
+        idTentativi.innerHTML = tentativi;
         tentativi++;
         this.removeEventListener("click", affonda);
+    }
+
+    function disabilataCelle() {
+        for(let i = 0; i < 10; i++)
+        {
+            for(let j = 0; j < 10; j++)
+            {
+                let cella = document.getElementById(i + "-" + j);
+                cella.removeEventListener("click", affonda);
+            }
+        }
     }
     
 }
